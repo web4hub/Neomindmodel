52952a6ba50332b675fb709f2b9ccbbdc9889be2
web4 <lunghugh@gmail.com>
2026-09-25 18:34:11 +0100

    Update and rename achieve.bashrsync to rsync.bash

--- rsync.bash
+++ rsync.bash
@@ -1,1 +1,2 @@
 rsync -avkSH --delete --delete-after --exclude=archive/debuginfo --exclude=archive/obsolete --exclude=SRPMS rsync://rsync.scientificlinux.org/scientific/obsolete/7x /export/pub/linux/scientific/obsolete/7x 
+
