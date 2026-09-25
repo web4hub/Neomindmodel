36061f41a42b05c0bb30177e127d1da5a8f26e15
web4 <lunghugh@gmail.com>
2026-09-25 17:16:35 +0100

    Create .nix

---
+++ .venv/etc/bin/.nix
@@ -0,0 +1,5 @@
+nix-env --install autoconf automake libtool pkg-config
+brew install autoconf automake libtool pkg-config
+sudo port install gettext autoconf automake libtool pkgconfig
+sudo yum install autoconf automake libtool pkg-config
+conda install autoconf automake libtool pkg-config
